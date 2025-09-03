e88aab785aadd5a39d026abae6c9edb3ce5dd305
ella :) <ejjohanknecht@willamette.edu>
2025-09-03 13:53:33 -0700

    Karel_Painting.py

--- Karel_Painting.py
+++ Karel_Painting.py
@@ -1,19 +1,47 @@
 #====================================================
 # Filename: Karel_Painting.py
 # 
-# Your name:
+# Your name: Ella Johanknecht
 # Who did you work with (if anyone)?:
 # If you consulted AI, link to transcript: 
-# Estimate for time spent on this prob (in hrs)?:
+# Estimate for time spent on this prob (in hrs)?: 0.5
 #====================================================
-
-# I've just laid out a basic starting function below, but remember that you
-# absolutely should define more helping functions to decompose the problem
-# into smaller pieces! Here I'm leaving those pieces (and helper functions)
-# up to you to design and name as you see fit. Don't forget comments!
 
 import karel
 
 def main():
-    """ Function to cause Karel to paint 3 sides of its house and then go indoors. """
-    # You can add your code below
+    orient_left()
+    for i in range(3):
+        paint_wall()
+    go_home()
+    yippee()
+
+def orient_left():
+# i've only seen karel spawn facing right but others have mentioned 
+# seeing her spawn facing any direction so I wanted to cover any bases
+    while left_is_clear():
+        if right_is_blocked():
+            for i in range(2):
+                turn_left()
+        if left_is_clear() and right_is_clear():
+            turn_left()
+
+def paint_wall():
+    move()
+    put_beeper()
+    move()
+    move()
+    turn_left()
+    move()
+
+def go_home():
+# the "paint_wall" functions get karel around the corner onto the 
+# wall with their door, but this will make karel go inside
+    move()
+    turn_left()
+    move()
+    move()
+
+def yippee():
+    for i in range(6):
+        turn_left()
