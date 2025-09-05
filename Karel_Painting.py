c34ea77e59873398ab8c1867966416f05a4bc0e6
ella :) <ejjohanknecht@willamette.edu>
2025-09-05 11:10:55 -0700

    Karel_Painting.py

--- Karel_Painting.py
+++ Karel_Painting.py
@@ -1,19 +1,53 @@
 #====================================================
 # Filename: Karel_Painting.py
 # 
-# Your name:
+# Your name: Ella Johanknecht
 # Who did you work with (if anyone)?:
 # If you consulted AI, link to transcript: 
-# Estimate for time spent on this prob (in hrs)?:
+# Estimate for time spent on this prob (in hrs)?: 0.75
 #====================================================
-
-# I've just laid out a basic starting function below, but remember that you
-# absolutely should define more helping functions to decompose the problem
-# into smaller pieces! Here I'm leaving those pieces (and helper functions)
-# up to you to design and name as you see fit. Don't forget comments!
 
 import karel
 
 def main():
-    """ Function to cause Karel to paint 3 sides of its house and then go indoors. """
-    # You can add your code below
+    orient_left()
+    for i in range(3):
+        paint_wall()
+    go_inside()
+    yippee()
+
+
+def orient_left():
+# this ensures karel paints their house counterclockwise
+    while left_is_clear():
+        if right_is_blocked():
+            for i in range(2):
+                turn_left()
+        if left_is_clear() and right_is_clear():
+            turn_left()
+
+def paint_wall():
+# i wasn't sure how to put the beepers in a pretty or symmetrical way 
+# along the walls, but this accomplishes the same thing
+    put_beeper()
+    while left_is_blocked():
+        if front_is_clear():
+            put_beeper()
+            move()
+    if left_is_clear():
+        turn_left()
+        move()
+
+def go_inside():
+# gets karel inside their house with enough room for karel to celebrate
+    while left_is_blocked():
+        if front_is_clear():
+            move()
+    if left_is_clear():
+        turn_left()
+        move()
+        move()
+
+def yippee():
+    for i in range(6):
+        turn_left()
